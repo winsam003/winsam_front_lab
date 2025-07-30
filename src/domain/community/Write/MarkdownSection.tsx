8f6a332b5710024b29a69b64241955e6ab429421
winsam003 <winsam003@gmail.com>
2025-07-30 16:51:40 +0900

    단축키 추가

--- src/domain/community/Write/MarkdownSection.tsx
+++ src/domain/community/Write/MarkdownSection.tsx
@@ -1,11 +1,17 @@
 import ReactMarkdown from "react-markdown";
-import remarkBreaks from "remark-breaks";
 import remarkGfm from "remark-gfm";
-import rehypeRaw from "rehype-raw";
 import { FormField, FormItem } from "@/components/ui/form";
 import { useFormContext } from "react-hook-form";
 import { Input } from "@/components/ui/input";
-import { useCallback, useState } from "react";
+import { useCallback, useRef } from "react";
+import { HiOutlineHashtag } from "react-icons/hi";
+import { HiOutlineBold } from "react-icons/hi2";
+import { FiItalic } from "react-icons/fi";
+import { HiOutlineCodeBracketSquare } from "react-icons/hi2";
+import { HiOutlineLink } from "react-icons/hi";
+import { HiOutlineListBullet } from "react-icons/hi2";
+import { HiOutlineMinusSm } from "react-icons/hi";
+import { HiOutlineUpload } from "react-icons/hi";
 
 const MarkdownSection = () => {
     const { register, watch, setValue } = useFormContext();
@@ -53,6 +59,94 @@ const MarkdownSection = () => {
         e.preventDefault();
     };
 
+    const markdownFunction = (key: string) => {
+        // 기존 텍스트 불러오기
+        const oldText = watch("post_cnts") || "";
+        let markdownFunction = "";
+
+        switch (key) {
+            case "h1":
+                markdownFunction = "#";
+                break;
+            case "h2":
+                markdownFunction = "##";
+                break;
+            case "h3":
+                markdownFunction = "###";
+                break;
+            case "bold":
+                markdownFunction = "**굵게**";
+                break;
+            case "italic":
+                markdownFunction = "*기울임*";
+                break;
+            case "codeblock":
+                markdownFunction = "```\n코드\n```";
+                break;
+            case "link":
+                markdownFunction = "[링크텍스트](https://)";
+                break;
+            case "list":
+                markdownFunction = "- 리스트 항목";
+                break;
+            case "hr":
+                markdownFunction = "---";
+                break;
+            default:
+                break;
+        }
+        // 마크다운 문법 삽입
+        const newText = oldText + `\n\n${markdownFunction} `;
+        setValue("post_cnts", newText);
+
+        // 포커스 전환
+        setTimeout(() => {
+            if (textareaRef.current) {
+                textareaRef.current.focus();
+                textareaRef.current.selectionStart = textareaRef.current.selectionEnd = textareaRef.current.value.length;
+            }
+        }, 0);
+    };
+
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleFileIconClick = () => {
+        inputRef.current?.click();
+    };
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        if (!files || files.length === 0) return;
+
+        uploadFile(files[0]);
+    };
+    const uploadFile = async (file: File) => {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        try {
+            const API_URL = import.meta.env.VITE_APP_API_URL;
+            const res = await fetch(`${API_URL}file/upload`, {
+                method: "POST",
+                body: formData,
+            });
+
+            if (!res.ok) throw new Error("업로드 실패");
+
+            const data = await res.json();
+            const imageUrl = data.url;
+
+            const oldText = watch("post_cnts") || "";
+            const newText = oldText + `\n\n![](${imageUrl})\n`;
+
+            setValue("post_cnts", newText);
+            setValue("thumbnail", imageUrl || "");
+        } catch (error) {
+            alert("이미지 업로드 실패");
+            console.error(error);
+        }
+    };
+
     return (
         <div>
             <div className="flex flex-col gap-4 mt-4 mb-4">
@@ -63,10 +157,86 @@ const MarkdownSection = () => {
                     <FormField name="reg_user" render={({ field }) => <Input {...field} placeholder="작성자" />} />
                     <FormField name="reg_dttm" render={({ field }) => <Input {...field} placeholder="작성일" disabled />} />
                     <FormField name="read_cnt" render={({ field }) => <Input {...field} placeholder="조회수" disabled />} />
+                </div>
+                <div>
                     <FormField name="imageUrl" render={({ field }) => <Input {...field} placeholder="썸네일" hidden />} />
+                    <input type="file" ref={inputRef} className="hidden" onChange={handleFileChange} />
                 </div>
             </div>
-            <div className="border-t border-solid h-7"></div>
+            <div className="border-t border-solid h-10 flex items-center pr-4 pl-4 gap-2">
+                <HiOutlineHashtag
+                    className="cursor-pointer rounded-md p-1 hover:bg-gray-100 transition"
+                    size={25}
+                    onClick={() => {
+                        markdownFunction("h1");
+                    }}
+                />
+                <div
+                    className="cursor-pointer rounded-md pr-1 pl-1 mb-0.5 hover:bg-gray-100 transition flex items-center gap-1"
+                    onClick={() => markdownFunction("h1")}
+                >
+                    <span className="text-[14px] font-medium select-none">H1</span>
+                </div>
+                <div
+                    className="cursor-pointer rounded-md pr-1 pl-1 mb-0.5 hover:bg-gray-100 transition flex items-center gap-1"
+                    onClick={() => markdownFunction("h2")}
+                >
+                    <span className="text-[14px] font-medium select-none">H2</span>
+                </div>
+                <div
+                    className="cursor-pointer rounded-md pr-1 pl-1 mb-0.5 hover:bg-gray-100 transition flex items-center gap-1"
+                    onClick={() => markdownFunction("h3")}
+                >
+                    <span className="text-[14px] font-medium select-none">H3</span>
+                </div>
+                <HiOutlineBold
+                    className="cursor-pointer rounded-md p-1 hover:bg-gray-100 transition"
+                    size={25}
+                    onClick={() => {
+                        markdownFunction("bold");
+                    }}
+                />
+                <FiItalic
+                    className="cursor-pointer rounded-md p-1 hover:bg-gray-100 transition"
+                    size={25}
+                    onClick={() => {
+                        markdownFunction("italic");
+                    }}
+                />
+                <HiOutlineCodeBracketSquare
+                    className="cursor-pointer rounded-md p-1 hover:bg-gray-100 transition"
+                    size={25}
+                    onClick={() => {
+                        markdownFunction("codeblock");
+                    }}
+                />
+                <HiOutlineLink
+                    className="cursor-pointer rounded-md p-1 hover:bg-gray-100 transition"
+                    size={25}
+                    onClick={() => {
+                        markdownFunction("link");
+                    }}
+                />
+                <HiOutlineListBullet
+                    className="cursor-pointer rounded-md p-1 hover:bg-gray-100 transition"
+                    size={25}
+                    onClick={() => {
+                        markdownFunction("list");
+                    }}
+                />
+                <HiOutlineMinusSm
+                    className="cursor-pointer rounded-md p-1 hover:bg-gray-100 transition"
+                    size={25}
+                    onClick={() => {
+                        markdownFunction("hr");
+                    }}
+                />
+                <HiOutlineUpload
+                    className="cursor-pointer rounded-md p-1 hover:bg-gray-100 transition"
+                    size={25}
+                    onClick={handleFileIconClick}
+                />
+            </div>
 
             <div className="flex gap-5 mb-4">
                 <div className="flex-1">
@@ -76,11 +246,12 @@ const MarkdownSection = () => {
                         render={({ field }) => (
                             <FormItem>
                                 <textarea
-                                    className="min-h-[500px] font-4 p-4 rounded-2xl"
+                                    className="min-h-[1000px] font-4 p-4 rounded-2xl resize-none"
                                     {...field}
                                     placeholder="여기에 마크다운으로 글을 작성하세요..."
                                     onDrop={handleDrop}
                                     onDragOver={handleDragOver}
+                                    ref={textareaRef}
                                 />
                             </FormItem>
                         )}
@@ -88,7 +259,7 @@ const MarkdownSection = () => {
                 </div>
 
                 {/* 미리보기 */}
-                <div className="flex-1 h-[500px] p-2.5 border border-gray-300 overflow-y-auto bg-gray-50 rounded-2xl">
+                <div className="flex-1 h-[1000px] p-2.5 border border-gray-300 overflow-y-auto bg-gray-50 rounded-2xl">
                     <ReactMarkdown
                         remarkPlugins={[remarkGfm]}
                         // rehypePlugins={[rehypeRaw]}
@@ -101,6 +272,9 @@ const MarkdownSection = () => {
                             ol: ({ node, ...props }) => <ol className="list-decimal ml-6" {...props} />,
                             li: ({ node, ...props }) => <li className="mb-1" {...props} />,
                             hr: () => <hr className="my-4 border-t border-gray-300" />,
+                            a: ({ node, ...props }) => (
+                                <a className="text-blue-600 hover:underline hover:text-blue-800 transition" {...props} />
+                            ),
                         }}
                     >
                         {watch("post_cnts")}
