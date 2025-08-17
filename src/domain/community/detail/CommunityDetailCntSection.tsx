589375ea01715b02f9c7e96fc365d39fd02ae58b
[winsam] <winsam003@gmail.com>
2025-08-18 00:37:59 +0900

    상세에 인용 등 추가

--- src/domain/community/detail/CommunityDetailCntSection.tsx
+++ src/domain/community/detail/CommunityDetailCntSection.tsx
@@ -20,7 +20,16 @@ const CommunityDetailCntSection = ({ postDetail }: props) => {
                         h2: ({ node, ...props }) => <h2 className="text-2xl font-semibold mb-3" {...props} />,
                         h3: ({ node, ...props }) => <h3 className="text-xl font-medium mb-2" {...props} />,
                         p: ({ node, ...props }) => <p className="mb-2" {...props} />,
-                        li: ({ node, ...props }) => <li className="list-disc ml-6" {...props} />,
+                        ul: ({ node, ...props }) => <ul className="list-disc ml-6" {...props} />,
+                        ol: ({ node, ...props }) => <ol className="list-decimal ml-6" {...props} />,
+                        li: ({ node, ...props }) => <li className="mb-1" {...props} />,
+                        hr: () => <hr className="my-4 border-t border-gray-300" />,
+                        a: ({ node, ...props }) => (
+                            <a className="text-blue-600 hover:underline hover:text-blue-800 transition" {...props} />
+                        ),
+                        blockquote: ({ node, ...props }) => (
+                            <blockquote className="border-l-4 border-blue-500 bg-blue-50 text-blue-900 p-4 my-4 rounded-md" {...props} />
+                        ),
                     }}
                 >
                     {postDetail && postDetail.bbs_post_cnts}
